a4bd1bd375ccc118ac109d8f264da70babc1d476
TotoroDavid <davidtoro65@hotmail.com>
2021-05-07 11:59:58 -0500

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind')
+    // console.log(data);
+    // sayHi('david')
+    // sayHi(names.peter)
+    // sayHi(names.john)
