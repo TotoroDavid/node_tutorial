a2e2d50ffade394155896f4234aeed8483ed0224
TotoroDavid <davidtoro65@hotmail.com>
2021-05-13 16:47:55 -0500

    node-expressJs

--- 02-express-tutorial/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Home page !!!</h1>')
-})
-
-server.listen(5000)
