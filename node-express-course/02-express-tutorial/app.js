7b11cc96f8feeaad88315819ce4650cf27b1757c
TotoroDavid <davidtoro65@hotmail.com>
2021-05-22 19:26:00 -0500

    expressBasics

--- node-express-course/02-express-tutorial/app.js
+++ node-express-course/02-express-tutorial/app.js
@@ -1,56 +1,20 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// get all files 
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+// setup static and middleware
+app.use(express.static('./public'))
 
-const server = http.createServer((req, res) => {
-    // console.log(req.method);
-    // console.log(req.url);
-    //home page !!
-    const url = req.url
-    console.log(url);
-    if (url === '/') {
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(homePage)
-        res.end()
-    }
-    //about page!!
-    else if (url === '/about') {
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write('<h1>About page !!</h1>')
-        res.end()
-    }
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//  adding to static assets
+//  SSR
+// })
 
-    //styles
-    else if (url === '/styles.css') {
-        res.writeHead(200, { 'content-type': 'text/css' })
-        res.write(homeStyles)
-        res.end()
-    }
-    //image / logo 
-    else if (url === '/logo.svg') {
-        res.writeHead(200, { 'content-type': 'image/svg+xml' })
-        res.write(homeImage)
-        res.end()
-    }
-    //logic
-    else if (url === '/browser-app.js') {
-        res.writeHead(200, { 'content-type': 'text/javascript' })
-        res.write(homeLogic)
-        res.end()
-    }
-
-    //404
-    else {
-        res.writeHead(400, { 'content-type': 'text/html' })
-        res.write('<h1>page no found </h1>')
-        res.end()
-    }
-
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found')
 })
 
-server.listen(5000)
+app.listen(5000, () => {
+    console.log('server is listening on port 5000....')
+})
